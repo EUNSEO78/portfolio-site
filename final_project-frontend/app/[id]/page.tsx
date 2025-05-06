910d4ffa96f8c04c332feb64bb3432050f110ce5
EUNSEO78 <dmstjalstj4@gmail.com>
2025-05-06 20:40:56 +0900

    real final commit

--- final_project-frontend/app/[id]/page.tsx
+++ final_project-frontend/app/[id]/page.tsx
@@ -2,8 +2,8 @@ import GuestbookDetail from "./GuestbookDetail";
 import { getGuestbook } from "@/utils/api";
 
 const Page = async ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-  const guestbook = await getGuestbook(id);
+  // const { id } = params;
+  const guestbook = await getGuestbook(params.id);
   return <GuestbookDetail guestbook={guestbook} />;
 };
 
