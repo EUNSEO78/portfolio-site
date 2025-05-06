d72fab3f3a040162e62acb7aab4841aa4c9fed32
EUNSEO78 <dmstjalstj4@gmail.com>
2025-05-06 20:49:35 +0900

    distribution final

--- final_project-frontend/app/[id]/page.tsx
+++ final_project-frontend/app/[id]/page.tsx
@@ -1,10 +1,14 @@
 import GuestbookDetail from "./GuestbookDetail";
 import { getGuestbook } from "@/utils/api";
 
-const Page = async ({ params }: { params: { id: string } }) => {
-  // const { id } = params;
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+const Page = async ({ params }: PageProps) => {
   const guestbook = await getGuestbook(params.id);
   return <GuestbookDetail guestbook={guestbook} />;
 };
-
 export default Page;
