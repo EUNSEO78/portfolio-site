de16a17dafa01db7df758c8eb540749395238b5e
EUNSEO78 <dmstjalstj4@gmail.com>
2026-05-06 22:03:42 +0900

    fix: update CORS origin for production

--- final_project-backend/src/main.ts
+++ final_project-backend/src/main.ts
@@ -6,7 +6,7 @@ import { TransformInterceptor } from './transform/transform.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: ['http://localhost:3000', 'portfolio-site-two-swart.vercel.app'],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
     credentials: true,
   });
