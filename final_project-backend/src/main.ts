51a5d515893ce738439eff2925dea4b7c582cac0
EUNSEO78 <dmstjalstj4@gmail.com>
2026-05-06 22:21:20 +0900

    fix: add https to API URL for production

--- final_project-backend/src/main.ts
+++ final_project-backend/src/main.ts
@@ -6,7 +6,10 @@ import { TransformInterceptor } from './transform/transform.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'portfolio-site-two-swart.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://portfolio-site-two-swart.vercel.app',
+    ],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
     credentials: true,
   });
